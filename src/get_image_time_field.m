b8b0ad5b5e1ad68dd1f6ef3c03430dd243baf369
Sophie Dixneuf <sophie.dixneuf@bioaster.org>
2019-08-20 17:04:28 +0200

    Change cutting character for finding time_number and field_number ('T'-->'_T' and 'Y'-->'_XY').

--- src/get_image_time_field.m
+++ src/get_image_time_field.m
@@ -1,7 +1,9 @@
 function [field_number, time_number, time_to_track] = get_image_time_field(file)
 %% get_image_time_field 
-% Find field and time index in the image file name. The file name must
-%**************** A COMPLETER
+% Find field and time index in the brightfield image file name. The file name must contain 
+% the string '_T' followed by the time_number string in 2 digits
+% (ex:'_T05'), and the string '_XY' followed by the field_number string in
+% 3 digits (ex:'_XY004').
 % 
 %   Usage 
 % [field_number, time_number, time_to_track] = get_image_time_field(file)
@@ -22,11 +24,11 @@ function [field_number, time_number, time_to_track] = get_image_time_field(file)
 %   CREATED : 2019-08-20
 %--------------------------------------------------------------------------
 
-    index_t = strfind(file, 'T');
-    time_number = strcat( file(index_t+1), file(index_t+2) );
-    index_y = strfind(file, 'Y');
-    field_number = strcat( file(index_y+1), file(index_y+2), ...
-                           file(index_y+3) );
+    index_t = strfind(file, '_T');
+    time_number = strcat( file(index_t+2), file(index_t+3) );
+    index_y = strfind(file, '_XY');
+    field_number = strcat( file(index_y+3), file(index_y+4), ...
+                           file(index_y+5) );
     time_to_track = str2double(time_number)+1;
     if time_to_track < 10
         time_to_track = strcat( '0', char(string(time_to_track)) );
